292ae29adde2f53edb71fe5c09c25054633d1116
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-03-25 14:16:45 +0200

    add contrellers

--- src/server.js
+++ src/server.js
@@ -1,60 +1,38 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-//
+// DB
 import { connectMongoDB } from './db/connectMongoDB.js';
+// middlewares
+import { logger } from './middleware/logger.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+// routes
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-//: General middlewares
-app.use(express.json());
+//^ General middlewares
+app.use(logger);
+app.use(
+  express.json({ type: ['application/json', 'application/vnd.api+json'] }),
+);
 app.use(cors());
 
-//: Запуск сервера
+//^ Routs
+app.use(notesRoutes);
+
+//^ 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
+
+//^ Помилка під час запиту
+app.use(errorHandler);
+
+//^ DB Mongo
+await connectMongoDB();
+
+//^ Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-//: GET
-// notes
-
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
-
-// note ID
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  const id_param = noteId;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${id_param}`,
-  });
-});
-
-// ^ Обробка помилок
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
-app.use((err, req, res, next) => {
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Someting went wrong. Please try again later'
-      : err.message,
-  });
-});
-
-// ^ Mid-re 404 (після всіх маршрутів)
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// DB Mongo
-await connectMongoDB();
