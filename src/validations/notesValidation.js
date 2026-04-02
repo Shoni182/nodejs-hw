f3355c146909f539297d84e4e8a522bdfb5eee2b
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-04-02 17:45:41 +0300

    hotfix

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -20,7 +20,7 @@ export const noteIdSchema = {
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().trim().allow(''),
   }),
