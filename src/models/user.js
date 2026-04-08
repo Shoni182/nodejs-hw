91d944b9cb32cda4e5b27d4c27507490516fb820
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-04-08 20:01:25 +0300

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+import { Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLenght: 8,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre('save', function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
+});
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
